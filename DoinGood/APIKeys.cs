70435b8014410be7085e95887db3ca414fc7ac74
unknown <meekhunsutp@gmail.com>
2021-01-04 12:58:05 -0600

    Ignore APIKey

---
+++ DoinGood/APIKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoinGood
+{
+    public static class APIKeys
+    {
+        public static string GoogleApi = "";
+    }
+}
